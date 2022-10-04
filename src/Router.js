489a74cc2ed7c23ecb51ede0382ee67fb687f82c
ahn-sujin <asj9674@gmail.com>
2022-10-04 21:41:44 +0900

    Add: 검색기능 통신 준비 완료

--- src/Router.js
+++ src/Router.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main/Main";
 import Login from "./pages/Login/Login";
 import MyPage from "./pages/MyPage/MyPage";
+import Nav from "./components/Nav/Nav";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/main" element={<Main />} />
---
+++ src/components/Nav/Search.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+  // console.log("검색어:", search);
+  const navigate = useNavigate();
+
+  const handleSearchValue = e => {
+    const searchValue = e.target.value;
+    setSearchValue(searchValue);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    navigate("/main", { state: searchValue });
+    setSearchValue("");
+  };
+
+  return (
+    <SearchArea onSubmit={handleSubmit}>
+      <SearchBox onChange={handleSearchValue} value={searchValue} />
+    </SearchArea>
+  );
+};
+
+export default Search;
+
+const SearchArea = styled.form`
+  width: 80%;
+  margin: 0 10px;
+`;
+
+const SearchBox = styled.input.attrs({ placeholder: "검색" })`
+  width: 100%;
+  padding: 10px 20px 10px 45px;
+  border: none;
+  font-size: 16px;
+  background-color: #e9e9e9;
+  background-image: url(/images/search_icon.svg);
+  background-repeat: no-repeat;
+  background-position: 20px center;
+  background-size: 15px 15px;
+  border-radius: 20px;
+
+  &::placehoder {
+    color: ${({ theme }) => theme.style.deepGrey};
+  }
+
+  &:focus {
+    padding-left: 20px;
+    background-image: none;
+  }
+`;
