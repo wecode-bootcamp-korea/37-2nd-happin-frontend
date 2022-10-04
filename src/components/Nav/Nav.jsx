d0c0b697f3935cf19c6e3cf1fbf81b6929b88c5a
ahn-sujin <asj9674@gmail.com>
2022-10-04 21:41:44 +0900

    Add: 검색기능 통신 준비 완료

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,7 +1,78 @@
 import React from "react";
+import Search from "./Search";
+import styled from "styled-components";
 
 const Nav = () => {
-  return <div>Hi I'm Nav</div>;
+  return (
+    <NavLayout>
+      <NavWrap>
+        <Logo src="./images/happin_logo.jpeg" alt="로고" />
+        <HomeMenu>홈</HomeMenu>
+        <PinMenu>핀 만들기</PinMenu>
+        <Search />
+        <ProfileImg src="https://youth1.com/sites/all/themes/youth1_2016/images/avatar-default.jpg" />
+        <Logout />
+      </NavWrap>
+    </NavLayout>
+  );
 };
 
 export default Nav;
+
+const NavLayout = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 15px 30px;
+`;
+
+const NavWrap = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+  text-align: center;
+  border-radius: 50%;
+`;
+
+const HomeMenu = styled.div`
+  padding: 15px 20px;
+  color: #fff;
+  background-color: ${({ theme }) => theme.style.deepGrey};
+  border-radius: 40px;
+`;
+
+const PinMenu = styled.div`
+  padding: 0 10px;
+  color: ${({ theme }) => theme.style.deepGrey};
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const ProfileImg = styled.img`
+  width: 40px;
+  height: 40px;
+  margin: 0 10px;
+  border-radius: 50%;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+const Logout = styled.button`
+  width: 20px;
+  height: 20px;
+  padding: 0;
+  background: none;
+  border: none;
+
+  &::after {
+    content: "\f08b";
+    font-family: FontAwesome;
+    font-size: 20px;
+  }
+`;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Main = () => {
+  const { state } = useLocation();
+  // console.log(state);
+  console.log(`api주소?search=${state}`);
   return <div>Hi, I'm Main.</div>;
 };
 
