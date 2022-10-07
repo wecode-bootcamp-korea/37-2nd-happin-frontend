f5860bb76e06117ad1a21d454a989468eeb5bf8e
leeyujin <linoonil@hanmail.net>
2022-10-07 20:19:38 +0900

    modify :통신 후 메인으로 이동

--- src/config.js
+++ src/config.js
@@ -1,2 +1,8 @@
 const BASE_URL = "http://10.58.5.151:8000";
 export const GET_PRODUCT_API = `${BASE_URL}/products`;
+
+export const GET_INTEREST_INFO = `${BASE_URL}/user/interest`;
+
+export const POST_INTEREST_INFO = `${BASE_URL}/main/interest`;
+
+export const accessToken = localStorage.getItem("token");
